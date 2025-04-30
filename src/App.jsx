27ae081d97e644c6bd3388cb73d9ad28e8adb713
dhamodharan-dev <dhamu1908@email.com>
2025-04-30 11:47:16 +0530

    Version 1.1.0 (Not found page added)

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Home from "./pages/Home"
 import Login from "./pages/Login"
 import Register from "./pages/Register"
 import NavBar from "./layout/NavBar"
+import NotFoundPage from "./pages/NotFoundPage"
 
 function App() {
 
@@ -13,6 +14,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
+        <Route path="*" element={<NotFoundPage />} />
       </Routes>
     </main>
   )
---
+++ src/pages/NotFoundPage.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import "../styles/NotFoundPage.css"
+import { Link } from 'react-router-dom'
+
+function NotFoundPage() {
+  return (
+    <div className='not-found'>
+      <div className="not-found-text">
+        <h1 className='not-found-number'>404</h1>
+        <h3>page not found</h3>
+      </div>
+      <Link to="/"><p className='not-found-btn'>Back to main page</p></Link>
+    </div>
+  )
+}
+
+export default NotFoundPage
